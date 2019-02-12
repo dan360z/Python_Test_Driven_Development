bc08d48de8e9b89d796f69206f6f5cb05b15e3d3
Daniel Field <dannyf506@googlemail.com>
2019-02-12 20:33:02 +0000

    added new tests and modified code to make them pass

--- vending_machine.py
+++ vending_machine.py
@@ -4,7 +4,16 @@ def get_change(amount):
     if amount == 0:
         return []
     
-    return [amount]    
+    if amount in [100, 50, 20, 10, 5, 2, 1]:
+        return [amount] 
+        
+    change = []
+    for coin in [100, 50, 20, 10, 5, 2, 1]:
+        if coin <= amount:
+            amount -= coin
+            change.append(coin)
+            
+    return change
 
 test_are_equal(get_change(0), [])
 test_are_equal(get_change(1), [1])
@@ -14,6 +23,7 @@ test_are_equal(get_change(10), [10])
 test_are_equal(get_change(20), [20])
 test_are_equal(get_change(50), [50])
 test_are_equal(get_change(100), [100])
-
+test_are_equal(get_change(3), [2, 1])
+test_are_equal(get_change(7), [5, 2])
 
 print("All tests pass!")
